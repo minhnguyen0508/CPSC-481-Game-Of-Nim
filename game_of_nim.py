69339dfac3659a72b11911e59ba94b9fe19663d0
minhnguyen0508 <minhtnnguyen0508@gmail.com>
2025-10-17 20:38:45 -0700

    fixing bug that cause game didn't run in bigger size

--- game_of_nim.py
+++ game_of_nim.py
@@ -7,43 +7,54 @@ class GameOfNim(Game):
     a list with number of objects in each row."""
 
     def __init__(self, board=[3,1]):
-        self.board = board
-        moves=[]
+        """moves=[]
         for idx, move in enumerate(board):
             if move != 0:
                 i = 1
                 while i <= move:
                     moves.append((idx, i))
-                    i+=1
-        self.initial = GameState(to_move = 'MIN',utility = 0, board=board ,moves=moves) 
+                    i+=1"""
+        moves = self.get_moves(board)
+        self.initial = GameState(to_move='MAX', utility=0, board=board, moves=moves)
+ 
 
+    def get_moves(self, board): #helper function to add move to moves list
+        moves =[]
+        for i, move in enumerate(board):
+            for n in range(1, move +1):
+                moves.append((i,n))
+        return moves
+    
     def actions(self, state):
         """Legal moves are at least one object, all from the same row."""
 
         return state.moves
 
     def result(self, state, move):
-        if move not in state.moves:
-            return state
+        i, n = move #(1,3) i =1, 3 =n
         board =state.board.copy()
-        moves = list(state.moves)
-        moves.remove(move)
-        x = move[0]     #(1,3) ==> x =1
-        y = move[1]     #y = 3
-        board[x]-= y
-        if(board[x] < 0):
-            board[x] =0
-        return GameState(to_move=('MIN' if state.to_move == 'MAX' else 'MAX'),
-                         utility=0,
-                         board=board, moves=moves)
+        board[i] -= n #remove n matches from i row 
+
+        next_turn = 'MIN' if state.to_move == 'MAX' else 'MAX'
+        moves = self.get_moves(board) #get next legal moves
+
+        utility = 0
+        if self.terminal_test(GameState(next_turn,0,board,move)):
+            utility = -1 if state.to_move =='MAX' else 1
+
+        return GameState(to_move=next_turn,utility=utility, board=board, moves=moves)
 
 
     def utility(self, state, player):
         """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
         return state.utility if player == 'MAX' else -state.utility
+   
     def terminal_test(self, state):
         """A state is terminal if there are no objects left"""
-        return state.utility != 0 or len(state.moves) == 0
+        for x in state.board:
+            if x != 0:
+                return False
+        return True
     
 
     def display(self, state):
@@ -52,8 +63,8 @@ class GameOfNim(Game):
 
 
 if __name__ == "__main__":
-    nim = GameOfNim(board=[0, 5, 3, 1]) # Creating the game instance
-    #nim = GameOfNim(board=[7, 5, 3, 1]) # a much larger tree to search
+    #nim = GameOfNim(board=[0, 5, 3, 1]) # Creating the game instance
+    nim = GameOfNim(board=[7, 5, 3, 1]) # a much larger tree to search
     print(nim.initial.board) # must be [0, 5, 3, 1]
     print(nim.initial.moves) # must be [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2, 1), (2, 2), (2, 3), (3, 1)]
     print(nim.result(nim.initial, (1,3) ))
